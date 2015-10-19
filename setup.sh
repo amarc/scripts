435df3038e28221984095b69a8dcd417f01b1e6a
Amar Cosic <amar.cosic@gmail.com>
2015-10-20 00:00:47 +0200

    Small setup script for LAMP on Ubuntu 14.04

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 PKGS="apache2 apache2-bin apache2-data apache2-mpm-event apache2-suexec apache2-suexec-pristine libapache2-mod-fcgid php-pear php5-cgi php5-cli php5-common php5-curl php5-gd 
-php5-imagick php5-json php5-mcrypt php5-mysql php5-readline php5-sqlite php5-xmlrpc php5-xsl mariadb-client mariadb-client-5.5 mariadb-client-core-5.5 mariadb-common mariadb-server 
-mariadb-server-5.5 mariadb-server-core-5.5 bzip2 rsync dialog cron"
+php5-imagick php5-json php5-mcrypt php5-mysql php5-readline php5-sqlite php5-xmlrpc php5-xsl mariadb-server bzip2 rsync dialog cron"
 
-apt-get -y install $PKGS
+
+#MariaDB https://downloads.mariadb.org/mariadb/repositories
+
+apt-get install software-properties-common
+apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+add-apt-repository 'deb http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
+
+apt-get update && apt-get -y install $PKGS
